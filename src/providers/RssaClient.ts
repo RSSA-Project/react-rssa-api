5dfeccd7bdb3cddf07b6691cd889606be42c7884
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2025-07-02 15:35:31 -0400

    fixed return type for put

--- src/providers/RssaClient.ts
+++ src/providers/RssaClient.ts
@@ -3,7 +3,7 @@ export interface RssaClientInterface {
 	getParticipantId(): string | null;
 	get<T>(path: string): Promise<T>;
 	post<T1, T2>(path: string, data: T1): Promise<T2>;
-	put<T>(path: string, data: T): void;
+	put<T>(path: string, data: T): Promise<void>;
 }
 
 class RssaClient implements RssaClientInterface {
