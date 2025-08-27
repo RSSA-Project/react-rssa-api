18f1e78a89b4c89bc2c0c23311e64ff913bf650b
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2025-08-27 16:13:30 -0400

    fixed NewParticipant

--- src/types/rssa-types/particpants.types.ts
+++ src/types/rssa-types/particpants.types.ts
@@ -1,17 +1,18 @@
 export type NewParticipant = {
-	participant_type: string;
-	external_id: string;
-	current_step: string;
-	current_page: string | null;
-}
+    study_id: string;
+    participant_type: string;
+    external_id: string;
+    current_step: string;
+    current_page: string | null;
+};
 
 export type Participant = {
-	id: string;
-	study_id: string;
-	participant_type: string;
-	external_id: string;
-	condition_id: string;
-	current_step: string;
-	current_page: string | null;
-	date_created: string;
-}
+    id: string;
+    study_id: string;
+    participant_type: string;
+    external_id: string;
+    condition_id: string;
+    current_step: string;
+    current_page: string | null;
+    date_created: string;
+};
